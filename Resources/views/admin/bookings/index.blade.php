7847ce95a8b9d64e254c1d30b972dcdff6324ba2
yusidabcs <agusyusida@gmail.com>
2017-02-13 17:52:09 +0800

    update bug

--- Resources/views/admin/bookings/index.blade.php
+++ Resources/views/admin/bookings/index.blade.php
@@ -85,6 +85,7 @@
 @stop
 
 @section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
     <script type="text/javascript">
         $( document ).ready(function() {
             $(document).keypressAction({
