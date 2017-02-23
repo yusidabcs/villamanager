274183c32fddf4c9619d32feeb3c49d5b3c94486
yusidabcs <agusyusida@gmail.com>
2017-02-23 18:00:44 +0800

    fix conflict

--- Resources/views/admin/villas/partials/create-nontrans-fields.blade.php
+++ Resources/views/admin/villas/partials/create-nontrans-fields.blade.php
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
+
 <div class='form-group{{ $errors->has('name') ? ' has-error' : '' }} col-md-12'>
-=======
-<div class='form-group{{ $errors->has('name') ? ' has-error' : '' }}'>
->>>>>>> 7847ce95a8b9d64e254c1d30b972dcdff6324ba2
     {!! Form::label('name', trans('villamanager::villas.form.name')) !!}
     {!! Form::text('name', Input::old('name'), ['class' => 'form-control', 'placeholder' => trans('villamanager::villas.form.name')]) !!}
     {!! $errors->first('name', '<span class="help-block">:message</span>') !!}
 </div>
-<<<<<<< HEAD
 <div class='form-group{{ $errors->has('villa_category') ? ' has-error' : '' }} col-md-4'>
     {!! Form::label('villa_category', trans('villamanager::villas.form.category')) !!}
     <select name="category_id" class="form-control">
@@ -23,17 +19,9 @@
 <div class="clearfix"></div>
 
 <div class="checkbox col-md-4" style="margin-left: 20px">
-=======
-
-<div class="checkbox" style="margin-left: 20px">
->>>>>>> 7847ce95a8b9d64e254c1d30b972dcdff6324ba2
     <label>
         <input type="checkbox" value="1" name="featured">
         {{ trans('villamanager::villas.form.featured') }}
     </label>
 </div>
 {!! $errors->first('featured', '<span class="help-block">:message</span>') !!}
-<<<<<<< HEAD
-
-=======
->>>>>>> 7847ce95a8b9d64e254c1d30b972dcdff6324ba2
